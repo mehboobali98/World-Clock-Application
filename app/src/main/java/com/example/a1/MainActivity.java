79c785a9dd5be1ed909bf13e2ea8237295012740
mehboobali98 <57248707+mehboobali98@users.noreply.github.com>
2021-06-28 23:36:45 +0500

    refactor code

--- app/src/main/java/com/example/a1/MainActivity.java
+++ app/src/main/java/com/example/a1/MainActivity.java
@@ -5,9 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
